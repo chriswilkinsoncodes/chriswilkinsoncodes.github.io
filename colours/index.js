fa000426be16431eaaaca776aa106d56985ca1db
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2021-08-30 14:28:24 -0400

    added 4 equally sized panels to cover entire screen

--- colours/index.js
+++ colours/index.js
@@ -3,5 +3,6 @@ const body = document.querySelector('body');
 function changeBG() {
   // from https://css-tricks.com/snippets/javascript/random-hex-color/
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  // end css-tricks code
   body.style.backgroundColor = `#${randomColor}`;
 }
