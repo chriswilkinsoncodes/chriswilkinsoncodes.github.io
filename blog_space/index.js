a51b084efb428cf40946b9cd58b81711b4a171bb
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2021-09-20 16:03:36 -0400

    nav bar and styling

--- blog_space/index.js
+++ blog_space/index.js
@@ -1,25 +1,24 @@
-/**
+/*
  * Challenge:
- * 
+ *
  * Send a request to the JSON Placeholder API using `fetch`
  * URL: https://apis.scrimba.com/jsonplaceholder/posts
- * 
+ *
  * Documentation: https://jsonplaceholder.typicode.com/
- * 
- * Log the response data to the console
  */
 
-let posts = ``
-
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(response => response.json())
-    .then(data => 
-        data.slice(0,5).forEach((obj) => {
-          posts += `<li>${obj.title}
+fetch('https://apis.scrimba.com/jsonplaceholder/posts')
+  .then((response) => response.json())
+  .then((data) => {
+    let posts = ``;
+    data.slice(0, 5).forEach((obj) => {
+      posts += `<li class="title">${obj.title}
                       <ul>
-                        <li>${obj.body}</li>
+                        <li class="body">${obj.body}</li>
                       </ul>
-                    </li>`
-        }))
-        
-setTimeout(function(){(document.getElementById('titles').innerHTML = posts)}, 500)
+                    </li>`;
+      setTimeout(function () {
+        document.getElementById('titles').innerHTML = posts;
+      }, 500);
+    });
+  });
