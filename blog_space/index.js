1f5bc68516f2a9316281be015745d12605ccacae
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2021-09-21 12:44:16 -0400

    submit post and render to DOM

--- blog_space/index.js
+++ blog_space/index.js
@@ -7,18 +7,44 @@
  * Documentation: https://jsonplaceholder.typicode.com/
  */
 
+let posts = ``;
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
-  .then((response) => response.json())
-  .then((data) => {
-    let posts = ``;
+  .then(response => response.json())
+  .then(data => {
     data.slice(0, 5).forEach((obj) => {
-      posts += `<li class="title">${obj.title}
+      posts += `<li class="post-title">${obj.title}
                       <ul>
-                        <li class="body">${obj.body}</li>
+                        <li class="post-body">${obj.body}</li>
                       </ul>
                     </li>`;
-      setTimeout(function () {
-        document.getElementById('titles').innerHTML = posts;
-      }, 500);
+                    document.getElementById('titles').innerHTML = posts;
     });
   });
+
+  document.getElementById('new-post').addEventListener('submit', function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById('post-title').value
+    const postBody = document.getElementById('post-body').value
+    const postData = {
+      title: postTitle,
+      body: postBody
+    }
+    console.log(postData.title, postData.body, postData)
+
+    fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res) => res.json())
+      .then((post) => {
+        posts =
+          `<li class="post-title">${post.title}
+            <ul>
+              <li class="post-body">${post.body}</li>
+            </ul>
+          </li>` + posts;
+
+        document.getElementById('titles').innerHTML = posts;
+      });
+  })
