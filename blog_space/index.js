23257925b3969092fece1d5b39f129b108f0b6d5
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2021-09-21 13:19:14 -0400

    refactor render to DOM

--- blog_space/index.js
+++ blog_space/index.js
@@ -1,50 +1,49 @@
-/*
- * Challenge:
- *
- * Send a request to the JSON Placeholder API using `fetch`
- * URL: https://apis.scrimba.com/jsonplaceholder/posts
- *
- * Documentation: https://jsonplaceholder.typicode.com/
- */
+//
+// @scrimba
+// URLs, REST, & BlogSpace
+//
 
-let posts = ``;
+let postsArr = []
+
+function renderPosts() {
+  let posts = ``;
+  postsArr.forEach(post => {
+    posts += `<li class="post-title">${post.title}
+                <ul>
+                  <li class="post-body">${post.body}</li>
+                </ul>
+              </li>`;
+      document.getElementById('titles').innerHTML = posts;
+  });
+}
+
+
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
   .then(response => response.json())
   .then(data => {
-    data.slice(0, 5).forEach((obj) => {
-      posts += `<li class="post-title">${obj.title}
-                      <ul>
-                        <li class="post-body">${obj.body}</li>
-                      </ul>
-                    </li>`;
-                    document.getElementById('titles').innerHTML = posts;
-    });
+    postsArr = data.slice(0, 5)
+    renderPosts()
   });
 
-  document.getElementById('new-post').addEventListener('submit', function(e) {
-    e.preventDefault()
-    const postTitle = document.getElementById('post-title').value
-    const postBody = document.getElementById('post-body').value
-    const postData = {
-      title: postTitle,
-      body: postBody
-    }
-    console.log(postData.title, postData.body, postData)
 
-    fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
-      method: 'POST',
-      body: JSON.stringify(postData),
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then((res) => res.json())
-      .then((post) => {
-        posts =
-          `<li class="post-title">${post.title}
-            <ul>
-              <li class="post-body">${post.body}</li>
-            </ul>
-          </li>` + posts;
+document.getElementById('new-post').addEventListener('submit', function(e) {
+  e.preventDefault()
+  const postTitle = document.getElementById('post-title').value
+  const postBody = document.getElementById('post-body').value
+  const postData = {
+    title: postTitle,
+    body: postBody
+  }
 
-        document.getElementById('titles').innerHTML = posts;
-      });
+  fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: { 'Content-Type': 'application/json' },
   })
+    .then((res) => res.json())
+    .then((post) => {
+      postsArr.unshift(post)
+      postsArr.pop()
+      renderPosts()
+    });
+})
