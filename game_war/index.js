eb7739b2aa95c0e26805bb14c6dafcbf4c967dcb
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2021-09-22 19:09:48 -0400

    display cards

--- game_war/index.js
+++ game_war/index.js
@@ -1,6 +1,13 @@
+
+const cardOne = document.getElementById('card1')
+const cardTwo = document.getElementById('card2')
+const cardThree = document.getElementById('card3')
+const cardFour = document.getElementById('card4')
+const cardFive = document.getElementById('card5')
+
 let deckId
-let card1
-let card2
+// let card1
+// let card2
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -12,13 +19,15 @@ function handleClick() {
 }
 
 function drawCards() {
-    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=8`)
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=5`)
         .then(res => res.json())
         .then(data => {
             if (data.success) {
-                card1 = data.cards[0].code
-                card2 = data.cards[1].code
-                console.log('cards: ', card1, card2)
+                cardOne.innerHTML = `<img src="${data.cards[0].image}" alt="card ${data.cards[0].code}">`
+                cardTwo.innerHTML = `<img src="${data.cards[1].image}" alt="card ${data.cards[1].code}">`
+                cardThree.innerHTML = `<img src="${data.cards[2].image}" alt="card ${data.cards[2].code}">`
+                cardFour.innerHTML = `<img src="${data.cards[3].image}" alt="card ${data.cards[3].code}">`
+                cardFive.innerHTML = `<img src="${data.cards[4].image}" alt="card ${data.cards[4].code}">`
             } else {
                 console.log('not enough cards')
             }
