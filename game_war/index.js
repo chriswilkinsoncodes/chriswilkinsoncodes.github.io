f94df6c39cde0fcc62f03f9f2b0314070f091620
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2021-09-22 22:11:44 -0400

    refactored display images; added background

--- game_war/index.js
+++ game_war/index.js
@@ -1,13 +1,8 @@
 
-const cardOne = document.getElementById('card1')
-const cardTwo = document.getElementById('card2')
-const cardThree = document.getElementById('card3')
-const cardFour = document.getElementById('card4')
-const cardFive = document.getElementById('card5')
+const imageContainer = document.querySelector('.image-container')
 
+const numCards = 2
 let deckId
-// let card1
-// let card2
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -15,25 +10,26 @@ function handleClick() {
         .then(data => {
             deckId = data.deck_id
             console.log(deckId)
+            imageContainer.innerHTML = ""
+            document.getElementById("draw").hidden = false;
             })
 }
 
 function drawCards() {
-    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=5`)
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=${numCards}`)
         .then(res => res.json())
         .then(data => {
             if (data.success) {
-                cardOne.innerHTML = `<img src="${data.cards[0].image}" alt="card ${data.cards[0].code}">`
-                cardTwo.innerHTML = `<img src="${data.cards[1].image}" alt="card ${data.cards[1].code}">`
-                cardThree.innerHTML = `<img src="${data.cards[2].image}" alt="card ${data.cards[2].code}">`
-                cardFour.innerHTML = `<img src="${data.cards[3].image}" alt="card ${data.cards[3].code}">`
-                cardFive.innerHTML = `<img src="${data.cards[4].image}" alt="card ${data.cards[4].code}">`
+                imageContainer.innerHTML = 
+                `<img src="${data.cards[0].image}" alt="card ${data.cards[0].code}">
+                <img src="${data.cards[1].image}" alt="card ${data.cards[1].code}">`
             } else {
                 console.log('not enough cards')
             }
         })
-
-}
-
+        
+    }
+    
+document.getElementById("draw").hidden = true;
 document.getElementById("new-deck").addEventListener("click", handleClick)
 document.getElementById("draw").addEventListener("click", drawCards)
