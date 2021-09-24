e86dad3a3b1d6da1376c2afe8e082273e78e5ef1
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2021-09-24 15:53:15 -0400

    refactored to use imageContainer.children instead of unique elements for each card

--- game_war/index.js
+++ game_war/index.js
@@ -1,18 +1,22 @@
 
 const imageContainer = document.querySelector('.image-container')
+const cardOne = document.getElementById('card-one')
+const cardTwo = document.getElementById('card-two')
 const cardsRemaining = document.getElementById('cards-remaining')
 
 const numCards = 2
 let deckId
 
-function handleClick() {
+function shuffleCards() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
             deckId = data.deck_id
             // console.log(deckId)
-            imageContainer.innerHTML = ""
-            document.getElementById("draw").hidden = false;
+            cardOne.innerHTML = ""
+            cardTwo.innerHTML = ""
+            document.getElementById("draw").style.visibility = 'visible';
+            document.getElementById("card-container").style.visibility = 'visible';
             cardsRemaining.innerText = `${data.remaining} cards remaining`
             })
 }
@@ -22,18 +26,27 @@ function drawCards() {
         .then(res => res.json())
         .then(data => {
             if (data.success) {
-                imageContainer.innerHTML = ""
+                // imageContainer.innerHTML = ""
                 for (let i = 0; i < numCards; i++) {
-                    imageContainer.innerHTML += 
+                    imageContainer.children[i].innerHTML = 
                     `<img src="${data.cards[i].image}" alt="card ${data.cards[i].code}">`
-                }
+
+                // cardOne.innerHTML =  `<img src="${data.cards[0].image}" alt="card ${data.cards[0].code}">`
+                // cardTwo.innerHTML =  `<img src="${data.cards[1].image}" alt="card ${data.cards[1].code}">`
+                
                 cardsRemaining.innerText = `${data.remaining} cards remaining`
+                }
             } else {
-                console.log('not enough cards')
+                displayWinner()
             }
         })
     }
     
-document.getElementById("draw").hidden = true;
-document.getElementById("new-deck").addEventListener("click", handleClick)
+    function displayWinner() {
+      console.log('not enough cards');
+    }
+    
+document.getElementById("draw").style.visibility = 'hidden';
+document.getElementById("card-container").style.visibility = 'hidden';
+document.getElementById("new-deck").addEventListener("click", shuffleCards)
 document.getElementById("draw").addEventListener("click", drawCards)
