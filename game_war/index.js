bc534c95bdfd9e30e8fb331ee5d11b84feedbb62
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2021-09-24 22:45:21 -0400

    game functionality completed

--- game_war/index.js
+++ game_war/index.js
@@ -1,24 +1,32 @@
 
+const drawBtn = document.getElementById("draw")
 const imageContainer = document.querySelector('.image-container')
 const cardOne = document.getElementById('card-one')
 const cardTwo = document.getElementById('card-two')
 const cardsRemaining = document.getElementById('cards-remaining')
-
+const displayWinnerMsg = document.querySelector('.display-winner')
+const computerScoreDisplay = document.querySelector('.computer-score')
+const playerScoreDisplay = document.querySelector('.player-score')
+const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
 const numCards = 2
-let deckId
+let deckId, hand
+let computerScore, playerScore
 
 function shuffleCards() {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-        .then(res => res.json())
-        .then(data => {
-            deckId = data.deck_id
-            // console.log(deckId)
-            cardOne.innerHTML = ""
-            cardTwo.innerHTML = ""
-            document.getElementById("draw").style.visibility = 'visible';
-            document.getElementById("card-container").style.visibility = 'visible';
-            cardsRemaining.innerText = `${data.remaining} cards remaining`
-            })
+  (computerScore = 0), (playerScore = 0);
+  fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
+    .then((res) => res.json())
+    .then((data) => {
+      deckId = data.deck_id;
+
+      for (let i = 0; i < numCards; i++) {
+        imageContainer.children[i].innerHTML = '';
+      }
+
+      document.getElementById('draw').style.visibility = 'visible';
+      document.getElementById('card-container').style.visibility = 'visible';
+      cardsRemaining.innerText = `${data.remaining} cards remaining`;
+    });
 }
 
 function drawCards() {
@@ -26,6 +34,7 @@ function drawCards() {
         .then(res => res.json())
         .then(data => {
             if (data.success) {
+                hand = data
                 // imageContainer.innerHTML = ""
                 for (let i = 0; i < numCards; i++) {
                     imageContainer.children[i].innerHTML = 
@@ -34,8 +43,12 @@ function drawCards() {
                 // cardOne.innerHTML =  `<img src="${data.cards[0].image}" alt="card ${data.cards[0].code}">`
                 // cardTwo.innerHTML =  `<img src="${data.cards[1].image}" alt="card ${data.cards[1].code}">`
                 
-                cardsRemaining.innerText = `${data.remaining} cards remaining`
                 }
+            cardsRemaining.innerText = `${data.remaining} cards remaining`
+            scoreHand()
+            if (!data.remaining) {
+                displayWinner()
+            }
             } else {
                 displayWinner()
             }
@@ -43,10 +56,33 @@ function drawCards() {
     }
     
     function displayWinner() {
-      console.log('not enough cards');
+      drawBtn.disabled = true;
+      if (computerScore > playerScore) {
+        displayWinnerMsg.innerText = 'Computer Wins the Game';
+      } else if (computerScore < playerScore) {
+        displayWinnerMsg.innerText = 'Player Wins the Game';
+      } else {
+        displayWinnerMsg.innerText = "It's a Tie Game";
+      }
+    }
+
+    function scoreHand() {
+        displayWinnerMsg.classList.remove('hidden')
+        if (cards.indexOf(hand.cards[0].value) > cards.indexOf(hand.cards[1].value)) {
+            displayWinnerMsg.innerText = "Computer Wins"
+            computerScore++
+            computerScoreDisplay.innerText = `Computer: ${computerScore}`
+        } else if (cards.indexOf(hand.cards[0].value) < cards.indexOf(hand.cards[1].value)) {
+            displayWinnerMsg.innerText = "Player Wins"
+            playerScore++
+            playerScoreDisplay.innerText = `Player: ${playerScore}`
+        } else {
+            displayWinnerMsg.innerText = "It's a Tie"
+        }
     }
     
+
 document.getElementById("draw").style.visibility = 'hidden';
 document.getElementById("card-container").style.visibility = 'hidden';
 document.getElementById("new-deck").addEventListener("click", shuffleCards)
-document.getElementById("draw").addEventListener("click", drawCards)
+drawBtn.addEventListener("click", drawCards)
