c5189702270a9512a06c4897e66e373ab285aa48
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2022-03-18 09:33:02 -0400

    full stack

--- business_card/components/About.js
+++ business_card/components/About.js
@@ -3,7 +3,7 @@ function About() {
         <section>
             <h2 className="section--title">About</h2>
             <p className="section--text">
-            I am a frontend developer with a particular interest in making things 
+            I am a full stack developer with a particular interest in making things 
             simple and automating daily tasks. I try to keep up with security and best 
             practices, and am always looking for new things to learn.
             </p>
