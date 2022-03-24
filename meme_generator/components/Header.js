7b48278105a19bc27d17cf62775a978bb36aa11f
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2022-03-24 15:50:21 -0400

    changed elements from nav to header

--- meme_generator/components/Header.js
+++ meme_generator/components/Header.js
@@ -1,9 +1,9 @@
 function Header(props) {
   return (
-    <nav>
-      <img src="./images/troll-face.png" className="nav--logo" />
-      <h1 className="nav--title">Meme Generator</h1>
-      <h2 className="nav--subtitle">React Course - Project 3</h2>
-    </nav>
+    <header>
+      <img src="./images/troll-face.png" className="header--logo" />
+      <h1 className="header--title">Meme Generator</h1>
+      <h2 className="header--subtitle">React Course - Project 3</h2>
+    </header>
   );
 }
