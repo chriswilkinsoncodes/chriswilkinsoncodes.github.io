0588d2c239281ee3811b03b1955da2b5f7149b9e
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2024-02-28 10:18:01 -0500

    updated punctuation

--- react_test/components/MainContent.js
+++ react_test/components/MainContent.js
@@ -3,10 +3,10 @@ function MainContent() {
       <main>
         <h1>Fun facts about Goldendoodles</h1>
         <ul>
-          <li>Social butterflies; great family dogs</li>
-          <li>High energy; love to roam and play</li>
-          <li>Ideal as service dogs</li>
-          <li>May also be known as Groodles</li>
+          <li>Social butterflies. Great family dogs.</li>
+          <li>High energy - love to roam and play.</li>
+          <li>Ideal as service dogs.</li>
+          <li>May also be known as Groodles.</li>
         </ul>
       </main>
     );
